05afac4f274a486c8b9191ad3d66679048138b08
JamesAndrewsXV <jamesandrewsxv@gmail.com>
2018-10-18 09:28:44 -0400

    Inheritance Implementation
    Beginning of enemy inheritence, minor changes (to work on later)

--- Enemy.h
+++ Enemy.h
@@ -2,15 +2,22 @@
 #define ENEMY_H
 
 #include "Stats.h"
+#include "Types.h"
+#include <vector>
+//#include loot table
 
+//Enemy class
 class Enemy
 {
 public:
 	Enemy();
 	~Enemy();
 
-private:
-	Stats stats;
+protected:
+	Stats *stats;
+	std::vector<Element> strengths;
+	std::vector<Element> weaknesses;
+	int enAttack(); // all enemies must have at least one basic attack.
 };
 
 
---
+++ Imp.h
@@ -0,0 +1,21 @@
+#ifndef IMP_H
+#define IMP_H
+
+#include "Stats.h"
+#include "Enemy.h"
+
+class Imp : Enemy
+{
+public:
+	Imp();
+	~Imp();
+
+protected:
+	Stats stats;
+};
+
+
+
+
+
+#endif // IMP_H
