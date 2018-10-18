aca4ab27b721f99e719896ff61230d267d92a851
glennacgreer <greer.g@husky.neu.edu>
2018-10-18 01:14:01 -0400

    Work on constructors

--- Item.cpp
+++ Item.cpp
@@ -7,18 +7,28 @@ using namespace std;
 class Item {
 	public:
 	//Creates an Item using given parameter
-	
+	Item(int damage, ItemType type, vector<Element, int> bonusDamage, vector<Stat, int> buffs, vector<Ability> abilities) {
+		damage = damage;
+		type = type;
+		bonusDamage = bonusDamage;
+		buffs = buffs;
+		abilities = abilities;
+	}
     //Creates an Item using random parameters
-	
+	Item randomItem() {
+		return new Item //Random inputs here
+	}	
 	//Destroys an Item
 	
 	//Deal damage with item
+	
 	private:
 	int damage;
 	ItemType type;
 	vector<Element, int> bonusDamage;
 	vector<Stat, int> buffs;
 	vector<Ability> abilities;
+	//Includes some sort of indication as to which sprite to use
 }
 
 enum ItemType{};
