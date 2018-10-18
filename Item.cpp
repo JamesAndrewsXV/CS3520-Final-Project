127614abadf964ba9a5b00c86aa6571fe9c52b43
glennacgreer <greer.g@husky.neu.edu>
2018-10-18 15:41:35 -0400

    Finished first pass on randomly generating items

--- Item.cpp
+++ Item.cpp
@@ -1,13 +1,44 @@
-#include Item.h
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
+
+#define NUMBER_ITEM_TYPES 3
+#define NUMBER_ELEMENTS 4
+#define NUMBER_STATS 2
+#define NUMBER_ABILITIES 5
+
+enum ItemType{
+	SWORD,
+	HAMMER,
+	MACE
+};
+
+enum Element{
+	FIRE,
+	WATER,
+	EARTH,
+	AIR
+};
+
+enum Stat{
+	BODY,
+	MIND
+};
+
+enum Ability{
+	WE,
+	WILL,
+	DO,
+	THESE,
+	LATER
+};
 
 class Item {
 	public:
 	//Creates an Item using given parameter
-	Item(int damage, ItemType type, vector<Element, int> bonusDamage, vector<Stat, int> buffs, vector<Ability> abilities) {
+	Item(int damage, ItemType type, vector<pair<Element, int> > bonusDamage, vector<pair<Stat, int> > buffs, vector<Ability> abilities) {
 		damage = damage;
 		type = type;
 		bonusDamage = bonusDamage;
@@ -15,26 +46,39 @@ class Item {
 		abilities = abilities;
 	}
     //Creates an Item using random parameters
-	Item randomItem() {
-		return new Item //Random inputs here
+	Item * randomItem() {
+		int tempDamage = (rand() % 10) + 1;
+		ItemType tempType = ItemType(rand() % NUMBER_ITEM_TYPES);
+		vector< pair<Element, int> > tempBonus(NUMBER_ELEMENTS);
+		
+		for (int i = 0; i < (rand() % NUMBER_ELEMENTS); i++) {
+			Element chosenElement = Element(rand() % NUMBER_ELEMENTS);
+      //Add later: make  it so that elements cannot be dupilicated
+      tempBonus.push_back(make_pair(chosenElement, rand() % 5));
+		}
+		
+		vector<pair<Stat, int> > tempBuffs(NUMBER_STATS);
+
+    for (int j = 0; j < (rand() % NUMBER_STATS); j++) {
+      Stat chosenStat = Stat(rand() % NUMBER_STATS);
+      tempBuffs.push_back(make_pair(chosenStat, rand() % 3));
+    }
+		vector<Ability> tempAbilities(NUMBER_ABILITIES);
+
+    for (int k = 0; k < (rand() % NUMBER_ABILITIES); k++) {
+      tempAbilities.push_back(Ability(rand() % NUMBER_ABILITIES));
+    }
+		return new Item(tempDamage, tempType, tempBonus, tempBuffs, tempAbilities);
 	}	
-	//Destroys an Item
+	//TODO: Destroys an Item
 	
-	//Deal damage with item
+	//TODO: Deal damage with item (dependent on how Player/Monsters work)
 	
 	private:
 	int damage;
 	ItemType type;
-	vector<Element, int> bonusDamage;
-	vector<Stat, int> buffs;
+	vector<pair<Element, int> > bonusDamage;
+	vector<pair <Stat, int> > buffs;
 	vector<Ability> abilities;
 	//Includes some sort of indication as to which sprite to use
-}
-
-enum ItemType{};
-
-enum Element{};
-
-enum Stat{};
-
-enum Ability{};
+};
