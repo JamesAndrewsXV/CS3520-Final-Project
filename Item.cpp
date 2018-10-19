ee2a79b50366e041e87b65ff7142113fcffc7e12
glennacgreer <greer.g@husky.neu.edu>
2018-10-19 00:51:08 -0400

    Added header file, added calculation of damage

--- Item.cpp
+++ Item.cpp
@@ -1,54 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
+
+#include <Stats.h>
 
 using namespace std;
 
-#define NUMBER_ITEM_TYPES 3
-#define NUMBER_ELEMENTS 4
-#define NUMBER_STATS 2
+#define NUMBER_ELEMENTS 7
+#define NUMBER_STATS 5
 #define NUMBER_ABILITIES 5
+#define NUMBER_RANGE 3
 
-enum ItemType{
-	SWORD,
-	HAMMER,
-	MACE
-};
 
-enum Element{
-	FIRE,
-	WATER,
-	EARTH,
-	AIR
-};
 
-enum Stat{
-	BODY,
-	MIND
-};
-
-enum Ability{
-	WE,
-	WILL,
-	DO,
-	THESE,
-	LATER
-};
-
-class Item {
-	public:
 	//Creates an Item using given parameter
-	Item(int damage, ItemType type, vector<pair<Element, int> > bonusDamage, vector<pair<Stat, int> > buffs, vector<Ability> abilities) {
+	Item::Item(int damage, Range range, vector<pair<Element, int> > bonusDamage, vector<pair<Buffs, int> > buffs, vector<Ability> abilities) {
 		damage = damage;
-		type = type;
+		range = range;
 		bonusDamage = bonusDamage;
 		buffs = buffs;
 		abilities = abilities;
 	}
     //Creates an Item using random parameters
-	Item * randomItem() {
+	Item * Item::randomItem() {
 		int tempDamage = (rand() % 10) + 1;
-		ItemType tempType = ItemType(rand() % NUMBER_ITEM_TYPES);
+		Range tempRange = Range(rand() % NUMBER_RANGE);
 		vector< pair<Element, int> > tempBonus(NUMBER_ELEMENTS);
 		
 		for (int i = 0; i < (rand() % NUMBER_ELEMENTS); i++) {
@@ -57,10 +30,10 @@ class Item {
       tempBonus.push_back(make_pair(chosenElement, rand() % 5));
 		}
 		
-		vector<pair<Stat, int> > tempBuffs(NUMBER_STATS);
+		vector<pair<Buffs, int> > tempBuffs(NUMBER_STATS);
 
     for (int j = 0; j < (rand() % NUMBER_STATS); j++) {
-      Stat chosenStat = Stat(rand() % NUMBER_STATS);
+      Buffs chosenStat = Buffs(rand() % NUMBER_STATS);
       tempBuffs.push_back(make_pair(chosenStat, rand() % 3));
     }
 		vector<Ability> tempAbilities(NUMBER_ABILITIES);
@@ -68,17 +41,17 @@ class Item {
     for (int k = 0; k < (rand() % NUMBER_ABILITIES); k++) {
       tempAbilities.push_back(Ability(rand() % NUMBER_ABILITIES));
     }
-		return new Item(tempDamage, tempType, tempBonus, tempBuffs, tempAbilities);
+		return new Item(tempDamage, tempRange, tempBonus, tempBuffs, tempAbilities);
 	}	
-	//TODO: Destroys an Item
+	//TODO(potentially): Destroys an Item
 	
-	//TODO: Deal damage with item (dependent on how Player/Monsters work)
+	//TODO: Deal damage with item (dependent on how Player/Monsters work
+	int Item::dealDamage() {
+		int tempDMG = 0;
+		tempDMG += damage;
+		for (int i = 0; i < bonusDamage.size; i++) {
+			tempDMG += bonusDamage[i]->second;
+		}
+		return tempDMG;
+	}
 	
-	private:
-	int damage;
-	ItemType type;
-	vector<pair<Element, int> > bonusDamage;
-	vector<pair <Stat, int> > buffs;
-	vector<Ability> abilities;
-	//Includes some sort of indication as to which sprite to use
-};
